fe6ee8f2be1c73d8181d8529b754efdb5ab9d89a
Shrikant Naidu <shrikantnaidu777@gmail.com>
2026-01-10 01:17:16 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const SocialIcon: React.FC<{ path: React.ReactNode; viewBox?: string }> = ({ pat
   <svg
     viewBox={viewBox}
     fill="currentColor"
-    className="w-5 h-5"
+    className="w-6 h-6"
     xmlns="http://www.w3.org/2000/svg"
   >
     {path}
